ab501659d5eb0789ce82246bf400e20245253ad4
ksider <semenov.ne@gmail.com>
2016-04-18 18:24:26 +0300

    News
    Пересобран вывод новости

--- single/single-else.php
+++ single/single-else.php
@@ -1,148 +1,159 @@
-<?php if( get_field('postphoto') ):?>
-	<style>
-		#navig.gettop,
-		#navig.gettop #searchform,
-		#navig.gettop #searchform input[type=search] {
-			background: transparent;
-			color: #fff;
-		}
-		
-		#navig.gettop a {
-			color: #fff;
-		}
-		
-		#navig.gettop .logoblock a.logo {
-			border-left: 4px solid #fff;
-		}
-		
-		#navig #searchform input[type=search] {
-			border: 1px solid #fff;
-		}
-		
-		#searchform input[type=submit] {
-			color: #fff;
-		}
-		
-		.newsheader {
-			position: relative;
-			padding: 0;
-			padding-top: 10%;
-			background-image: url(<?php the_field('postphoto');
-			?>);
-			background-size: cover;
-			background-repeat: no-repeat;
-			background-position: 50% 100%;
-			-webkit-box-shadow: 0 -12px 18px -15px rgba(0, 0, 0, 0.91) inset;
-			box-shadow: 0 -12px 18px -15px rgba(0, 0, 0, 0.91) inset;
-			color: #fff;
-			text-shadow: 1px 1px 2px #000;
-		}
-		
-		.newsheader a {
-			color: #f2f2f2;
-		}
-		
-		.newsheader > .block {
-			z-index: 99;
-			position: relative;
-		}
-		
-		.shadow {
-			position: absolute;
-			top: 0;
-			bottom: 0;
-			left: 0;
-			right: 0;
-			background-color: #313131;
-			opacity: 0.5;
-			z-index: 97;
-		}
-	</style>
-	<?php else : ?>
-		<style>
-			.newsheader {
-				padding-top: 3em;
-			}
-			
-			.shadow {
-				display: none;
-			}
-		</style>
-		<?php endif; ?>
+<link rel="stylesheet" href="<?php bloginfo("template_url"); ?>/template/css/science.css">
 
-			<div class="newsheader">
-				<div class="block">
-
-					<div class="singttl">
-						<?php the_title(); ?>
-					</div>
-					<div class="pure-social">
-						<div class="likely likely-small <?php if( get_field('postphoto') ): echo 'likely-light'; else : echo ' '; endif; ?>" data-single-title="Поделиться">
-							<div class="facebook" title="Поделиться ссылкой на Фейсбуке"></div>
-							<div class="twitter" title="Поделиться ссылкой в Твиттере"></div>
-							<div class="vkontakte" title="Поделиться ссылкой во Вконтакте"></div>
-							<div class="gplus" title="Поделиться ссылкой в Гугл-плюсе"></div>
-						</div>
-					</div>
-					<a id="navline"></a>
-				</div>
-				<div class="shadow"></div>
-			</div>
+<?php if( get_field('postphoto') ):
+    $image = get_field('postphoto'); ?>
+<style>
+.sc_single-head { background-image: url(<?php echo $image; ?>); }
+</style>
+<?php else : endif; ?>
 
 
+<div class="sc_single-txt sc_single-head">
+<div class="sc_single-title novideo">
 
-			<div class="block empcontent">
-		<div class="pure-g">
-				
-		
-				
-		<div class="pure-u-4-5">
-		<?php the_content(); ?>
-		</div>
-<div class="pure-u-1-5">
-	<div class="sideofnews">
-	<div class="sidedate">
-<p><?php the_time('d.m.Y '); ?></p>
-	</div>
+    <div class="sc_single-name">
+       <?php the_title(); ?>
+        <a href="/science" class="hprew">/Новости</a>
+        
+    </div>
+</div>
+    
+ 
+<div class="shadows"></div>
+</div>
+<div class="sc_single-footer">
+        
+       <div class="pure-g">
+       <div class="block">
+        <div class="pure-u-1-3">
+            
+        <div class="science-date">
+           <span><?php the_time('j F, Y | G:i'); ?></span>	 
+        </div>
+            
+        </div>
+  <div class="pure-u-1-3 center">
+
+    <div class="likely likely-small" data-single-title="Поделиться">
+		<div class="facebook" title="Поделиться ссылкой на Фейсбуке"></div>
+		<div class="twitter" title="Поделиться ссылкой в Твиттере"></div>
+		<div class="vkontakte" title="Поделиться ссылкой во Вконтакте"></div>
+		<div class="gplus" title="Поделиться ссылкой в Гугл-плюсе"></div>
+    </div>
+    
+ </div>
+        <div class="pure-u-1-3">
+            <div class="scs right">
+        <?php the_category(' '); ?>	
+            </div>
+       </div>
+       </div>
+    </div>  
+   
+        
+    </div>    
+
+ 
+
+<div class="sc_single-content" id="content">
+    
+<div class="block">
+       
+       <div class="pure-g">
+           <div class="pure-u-2-3">
+        <div class="sc_single-exerpt">
+            <?php the_excerpt(); ?>
+        </div>           
+        <div class="sc_single-block">
+            <?php the_content(); ?>
+        </div>
+          </div>
+           <div class="pure-u-1-3">
+              
+<?php 
+$poid = array( $post->ID );    
+if( has_term( '', 'post_tag', $post->ID ) ){
+?> 
+<div class="sc_single-related">
+
+    <div class="rblock">
+<h4 class="sc_single-header"><span>Похожие</span></h4>
+
+ <?php
+
+    $gettages = get_the_tags($post->ID);
+    $tags = array();
+    foreach($gettages as $tag){
+        $tag_id = $tag->term_id;
+        $tags[] = $tag_id;
+    }
+      
+    
+    $rel = array( 
 	
-	<div class="categories">
-<?php the_category(', '); ?>
-	</div>
+   'post__not_in' => $poid,
+    'post_type'=>'science',
+    'posts_per_page' => 3, 
+    'orderby'=> 'date',
+    'order'=> 'DESC', 
+        
+    'tax_query' => array(
+		'relation' => 'OR',
+		array(
+			'taxonomy' => 'post_tag',
+			'field' => 'id',
+			'terms' => $tags ,
+            'operator' => 'IN'
+		),
+	),
+    );     
+    $screl = get_posts( $rel );  
+foreach ($screl as $post) :  setup_postdata($post);
+?>
 
-	<div class="tags">
-	<?php the_tags('', ' / '); ?>
-	</div>
-	</div>
-	
-		</div>
-				</div>
-			</div>
+            
+<?php if( get_field('postphoto') ):
+    $image = get_field('postphoto'); ?>
+    <style>
+        .sc-related-pic.id<?php the_ID();?> {
+            background-image: url(<?php echo $image; ?>);
+        }
+    </style>
+<?php else : endif; ?>         
+
+            
+
+<a class="sc-related-block" href="<?php the_permalink(); ?>" class="">
+<div class="sc-related-pic id<?php the_ID();?>"></div>
+<div class="sc-related-title">
+<div class="sc-related-authors"><?php the_time('j F, Y | G:i'); ?></div>
+       <div class="sc-related-names"><?php the_title(); ?></div>
+</div>
+</a>
+           
+            
+            <?php wp_reset_postdata(); ?>
+            <?php endforeach;  ?>
+
+   
+   </div>
+</div>  
+<?php } ?>             
+               
+           </div>
+       </div>
+
+        
 
 
-			<!-- коменты -->
-			<div class="comm">
+</div>  
+</div>
 
-				<div class="pure-g">
-					<div class="block">
 
-						<div class="pure-u-1">
-							<h3>Комментарии:</h3>
-						</div>
 
-					</div>
-				</div>
-
-				<div class="block">
-					<div class="commblock">
-
-						<div class="pure-g">
-							<div class="pure-u-1">
-								<div class="block">
-<?php comments_template(); ?>
-								</div>
-							</div>
-						</div>
-
-					</div>
-				</div>
-			</div>
+<div class="science-single_comment">
+     <div class="block">
+     <h3 class="center">Коментарии</h3>
+     <?php comments_template(); ?>
+     </div>
+</div> 
