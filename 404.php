4784ed8c8afa8622733b6519e2450c7c7cfb754e
ksider <semenov.ne@gmail.com>
2016-04-22 00:22:29 +0300

    SlipNews

--- 404.php
+++ 404.php
@@ -5,12 +5,15 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 
 <head>
-<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?> />
+<meta http-equiv="Content-Type" content="<?php bloginfo("html_type"); ?> />
 <meta charset="UTF-8">
+
 <meta name="viewport" content="width=device-width, initial-scale=1">
+
 <title>
 404
 </title>
+
 <link rel="alternate" type="application/rss+xml" title="<?php get_bloginfo('name'); ?>" href="<?php bloginfo('rss2_url'); ?>" />
 
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
@@ -19,6 +22,11 @@
 
 <?php wp_head(); ?>
 
+
+
+
+</head>
+<body>
 <?php
   $bg = array('bg-01.jpg', 'bg-02.jpg', 'bg-03.jpg', 'bg-04.jpg', 'bg-05.jpg', 'bg-06.jpg', 'bg-07.jpg', 'bg-08.jpg', 'bg-09.jpg', 'bg-10.jpg', 'bg-11.jpg', 'bg-12.jpg' ); // array of filenames
   $ic = array('war', 'body-cut', 'unicorn', 'body-overlay', 'car-burn', 'death-boiling', 'electrical-shock', 'execute-hanging', 'fell-down', 'garbage', 'head-shot-arrow', 'head-shot', 'hippie', 'nuclear', 'frankenstein', 'skull', 'zynga', 'head-stab-1', 'head-cut' ); // array of filenames
@@ -29,58 +37,119 @@
   $selectedIc = "$ic[$z]"; 
 ?>
 
-<style type="text/css">
-
+<style>
+ 
 .page404 {
 	position:relative;
-	background:#c0392b  url(<?php bloginfo("template_url"); ?>/img/bc/<?php echo $selectedBg; ?>);
 	overflow:hidden;
 	height:100%;
-	background-size: cover;
-	background-repeat: no-repeat;}
-
-.nonfound a {color:#2ecc71;}
-.nonfound { 
-	background:rgba(44, 62, 80,0.5);
-	padding:2em;
-	top:25%; color:#ecf0f1;
-	width:100%;
-	margin:0 auto;
-	position:absolute; 
-	text-align:left;}
-.bigttl { 
-	line-height: 100%;
-	font-size:12em;
-	width:60%; margin:0 auto;}
-.ntftxt {
-	width:60%; 
-	font-size:18px;
-	margin:0 auto;}
-.nonfound .ntf-search {
-	width:200px; display:inline-block;
-	 margin:1em 0 0;
-	position:relative;}
-
+    
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    background-image: url('<?php bloginfo("template_url"); ?>/img/bc/<?php echo $selectedBg; ?>');
+  /*background-image: url('https://source.unsplash.com/1600x900/?patterns');
+    background-image: url('https://source.unsplash.com/category/technology/1600x900');
+    */
+    display: -webkit-flex;
+    display: -moz-flex;
+    display: -ms-flex;
+    display: -o-flex;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: center;
+    -webkit-justify-content: center;
+    -ms-flex-pack: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -webkit-align-items: center;
+    -ms-flex-align: center;
+    align-items: center;
+    
+    }
+    
+.p404p {
+        background-color: rgba(49,49,49,0.6);
+        padding:1em 2em;
+        padding-bottom: 0.5em;
+        font-size: 4em; 
+        font-weight: bold;
+        color: #fff;
+        text-align: center;
+    
+       /* background-image: url('https://c2.staticflickr.com/2/1495/26405211772_d299c54ee0.jpg');*/
+        background-size: cover;
+    background-repeat: no-repeat;
+    background-position: 50% 50%;
+    } 
+    
+.navigator {
+    display: -webkit-flex;
+    display: -moz-flex;
+    display: -ms-flex;
+    display: -o-flex;
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap; 
+    background-color: rgba(49, 49, 49,0.8);
+    
+    } 
+.navigator > a {
+    color: #fff;
+    text-align: center;
+    width: 50%;
+    padding: 1em 0;
+    display: block;
+    position: relative;
+    text-decoration: none;
+    }
+    .navigator > a:hover {
+    background-color: rgba(241, 196, 15,0.2);  
+    }    
+.navigator a span{
+    font-size: 1em;
+    
+    letter-spacing: 0.3em;
+    text-transform: uppercase;
+    }   
 </style>
-
-</head>
-<body>
-
-		
-		
-<div class="page404">
-<div class="nonfound">
-<div class="bigttl">
-<i class="icon-<?php echo $selectedIc; ?>"></i>
-</div>
-<div class="ntftxt">
-Неправильно набран адрес, или такой страницы больше не существует.<br>
-Можно вернуться на <a href="/">главную</a>, посмотреть в <a href="/category/news">новостях</a>, в наших <a href="/#otdely">отделах</a> и среди <a href="/#administration">руководства</a>.
-Можно просто почитать о <a href="/history">институте</a>, ну или воспользоваться <a href="/index.php?s=">поиском</a>.
-</div>
-	</div>
-	</div>
 	
-		<?php wp_footer(); ?>
+<div class="page404" id="rand">
+
+    <div class="bigttl">
+        <div class="p404p">
+            Оп-па&#8230;
+        </div>
+
+
+        <div class="navigator">
+
+
+            <a href="/">
+                <span>Главная</span>
+            </a>
+
+            <a href="<?php echo home_url('/category/news/'); ?>">
+                <span>Новости</span>
+            </a>              
+            <a href="<?php echo home_url("/science/"); ?>">
+                <span>Поп-наука</span>
+            </a>            
+            <a href="<?php echo home_url('/index.php?s='); ?>">
+                <span>Поиск</span>
+            </a>
+
+        
+        </div>
+
+    </div>
+
+</div>
+		
+		
+
+<?php wp_footer(); ?>
+     
     </body>
 </html>
